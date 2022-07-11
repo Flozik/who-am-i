ee5f1b1b5b51a2f391e916d02e5fbbf558cbf476
Flozik <nazaro4ek.code@gmail.com>
2022-07-11 10:47:12 +0300

    Fix problem with NotBlank and NotNull

--- src/test/java/com/eleks/academy/whoami/model/request/CharacterSuggestionTest.java
+++ src/test/java/com/eleks/academy/whoami/model/request/CharacterSuggestionTest.java
@@ -1,10 +1,8 @@
 package com.eleks.academy.whoami.model.request;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -12,7 +10,7 @@ import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CharacterSuggestionTest {
 
@@ -25,10 +23,10 @@ public class CharacterSuggestionTest {
 	}
 
 	@ParameterizedTest
-	@CsvSource({"c,character,Nickname length must be between 2 and 50!",
+	@CsvSource(value = {"c,character,Nickname length must be between 2 and 50!",
 			"123456789012345678901234567890123456789012345678901,character,Nickname length must be between 2 and 50!",
-			"  ,character,Nickname must not be blank!",
-			",character,Nickname must not be null!"})
+			"'  ',character,Nickname must not be blank!",
+			"NULL,character,Nickname must not be null!"}, nullValues = "NULL")
 	void nickNameAnnotationsValidationTest(String nickName, String character, String message) {
 		CharacterSuggestion suggestion = new CharacterSuggestion();
 		suggestion.setNickName(nickName);
@@ -40,10 +38,10 @@ public class CharacterSuggestionTest {
 	}
 
 	@ParameterizedTest
-	@CsvSource({"nickName,c,Character length must be between 2 and 50!",
+	@CsvSource(value = {"nickName,c,Character length must be between 2 and 50!",
 			"nickName,123456789012345678901234567890123456789012345678901,Character length must be between 2 and 50!",
-			"nickName,  ,Character must not be blank!",
-			"nickName,,Character must not be null!"})
+			"nickName,'  ',Character must not be blank!",
+			"nickName,NULL,Character must not be null!"}, nullValues = "NULL")
 	void characterAnnotationsValidationTest(String nickname, String character, String message) {
 		CharacterSuggestion suggestion = new CharacterSuggestion();
 		suggestion.setNickName(nickname);
