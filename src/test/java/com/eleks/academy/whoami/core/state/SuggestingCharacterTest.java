4f286eb30155b7ad8081c81a07b59cd9ec263e51
Flozik <nazaro4ek.code@gmail.com>
2022-07-11 15:15:31 +0300

    Resolving proposed reviewing

--- src/test/java/com/eleks/academy/whoami/core/state/SuggestingCharacterTest.java
+++ src/test/java/com/eleks/academy/whoami/core/state/SuggestingCharacterTest.java
@@ -79,7 +79,11 @@ public class SuggestingCharacterTest {
 				String val1 = entry.getValue().getCharacter();
 				String val2 = result.get(key).getCharacter();
 
+				String expectedNickName = players.get(entry.getKey()).getNickName();
+				String resultNickName = result.get(entry.getKey()).getNickName();
+
 				assertNotEquals(val1, val2);
+				assertEquals(expectedNickName, resultNickName);
 			}
 		}
 	}
@@ -150,75 +154,6 @@ public class SuggestingCharacterTest {
 			String movedPlayer = result.get("Player4").getCharacter();
 
 			assertNotEquals(enteredPlayer, movedPlayer);
-		}
-	}
-
-	@Test
-	void nickNameNotChangeAfterShufflingTest() {
-		Map<String, SynchronousPlayer> players = new HashMap<>(4);
-		SynchronousPlayer player1 = new PersistentPlayer("Player1");
-		SynchronousPlayer player2 = new PersistentPlayer("Player2");
-		SynchronousPlayer player3 = new PersistentPlayer("Player3");
-		SynchronousPlayer player4 = new PersistentPlayer("Player4");
-		SynchronousPlayer player5 = new PersistentPlayer("Player1");
-		SynchronousPlayer player6 = new PersistentPlayer("Player2");
-		SynchronousPlayer player7 = new PersistentPlayer("Player3");
-		SynchronousPlayer player8 = new PersistentPlayer("Player4");
-
-		CharacterSuggestion suggestion1 = new CharacterSuggestion();
-		suggestion1.setCharacter("Character1");
-		suggestion1.setNickName("NickName1");
-		CharacterSuggestion suggestion2 = new CharacterSuggestion();
-		suggestion2.setCharacter("Character2");
-		suggestion2.setNickName("NickName2");
-		CharacterSuggestion suggestion3 = new CharacterSuggestion();
-		suggestion3.setCharacter("Character3");
-		suggestion3.setNickName("NickName3");
-		CharacterSuggestion suggestion4 = new CharacterSuggestion();
-		suggestion4.setCharacter("Character4");
-		suggestion4.setNickName("NickName4");
-		CharacterSuggestion suggestion5 = new CharacterSuggestion();
-		suggestion5.setCharacter("Character5");
-		suggestion5.setNickName("NickName5");
-		CharacterSuggestion suggestion6 = new CharacterSuggestion();
-		suggestion6.setCharacter("Character6");
-		suggestion6.setNickName("NickName6");
-		CharacterSuggestion suggestion7 = new CharacterSuggestion();
-		suggestion7.setCharacter("Character7");
-		suggestion7.setNickName("NickName7");
-		CharacterSuggestion suggestion8 = new CharacterSuggestion();
-		suggestion8.setCharacter("Character8");
-		suggestion8.setNickName("NickName8");
-
-		player1.suggestCharacter(suggestion1);
-		player2.suggestCharacter(suggestion2);
-		player3.suggestCharacter(suggestion3);
-		player4.suggestCharacter(suggestion4);
-		player5.suggestCharacter(suggestion5);
-		player6.suggestCharacter(suggestion6);
-		player7.suggestCharacter(suggestion7);
-		player8.suggestCharacter(suggestion8);
-
-		players.put("Player1", player1);
-		players.put("Player2", player2);
-		players.put("Player3", player3);
-		players.put("Player4", player4);
-
-		var characters = new SuggestingCharacters(players);
-
-		Map<String, SynchronousPlayer> inputPlayersAndCharacters = new HashMap<>(4);
-		inputPlayersAndCharacters.put("Player1", player5);
-		inputPlayersAndCharacters.put("Player2", player6);
-		inputPlayersAndCharacters.put("Player3", player7);
-		inputPlayersAndCharacters.put("Player4", player8);
-
-		var result = characters.assignCharacters();
-
-		for (var q : players.entrySet()) {
-			String expectedNickName = players.get(q.getKey()).getNickName();
-			String resultNickName = result.get(q.getKey()).getNickName();
-
-			assertEquals(expectedNickName, resultNickName);
 		}
 	}
 
